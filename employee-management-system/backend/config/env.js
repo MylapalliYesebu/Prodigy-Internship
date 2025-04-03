14916ec70e48f8ac0609a509e9fde3463820e7d3
MYLAPALLI YESEBU <145589148+MylapalliYesebu@users.noreply.github.com>
2025-04-03 23:52:46 +0530

    delete EMS

--- employee-management-system/backend/config/env.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/employee-management",
-  JWT_SECRET: process.env.JWT_SECRET || "fallback-secret-key",
-};
--- employee-management-system/frontend/src/router/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const token = localStorage.getItem("token"); // Check if token exists
-
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
